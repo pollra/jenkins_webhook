dc0203289a24a091d2fca46c538c6d5aa7ec9e77
Seunggeun Lee <pollra32@gmail.com>
2020-02-10 10:41:14 +0900

    update test 02

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,7 +2,7 @@ package app;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("update #01");
+        System.out.println("update #02");
         System.out.println("젠킨스 테스트입니다.");
     }
 }
